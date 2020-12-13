f745e7e308622ed81f1ed23aa89c10d8afbb5bb4
ngocthanh <ngocthanh06@gmail.com>
2020-12-13 17:21:53 +0700

    row, header, banner

---
+++ src/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Nav() {
+    return (
+        <div className="nav">
+            <img 
+                className="nav__logo"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Logonfx.png/800px-Logonfx.png"
+                alt="Netflix Logo"
+            />
+        </div>
+    )
+}
+
+export default Nav;
--- src/Row.js
+++ src/Row.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import axios from './axios';
+import "./Row.css";
 
-function Row({ title, fetchUrl }) {
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl, isLargeRow }) {
     const [ movies, setMovies ] = useState([]);
 
     // A snippet of code which runs based on a specific condition/varaible
@@ -10,18 +13,25 @@ function Row({ title, fetchUrl }) {
         async function fetchData() {
             const request = await axios.get(fetchUrl)
             //"https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_networks=213",
-            console.log(request);
+            setMovies(request.data.results)
             return request;
         }
         fetchData();
-    }, []);
-
+    }, [fetchUrl]);
 
     return (
-        <div>
+        <div className="row">
             <h2>{ title }</h2>
-            {fetchUrl}
-            {axios}
+            <div className="row__posters">
+                {/*  */}
+                {movies.map(movie => (
+                    // "/mYsWyfiIMxx4HDm0Wck7oJ9ckez.jpg"
+                    <img 
+                    key={movie.id}
+                    className={`row__poster ${isLargeRow && "row__posterLarge"}`} 
+                    src={`${base_url}${ isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} />
+                ))}
+            </div>
         </div>
     );
 }
